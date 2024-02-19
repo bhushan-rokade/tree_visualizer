a529f1388368581d3db839bab3d7c82d1350ae00
Bhushan Rokade <83108525+http-bhushan@users.noreply.github.com>
2024-02-19 14:04:42 +0530

    expression tree page added

--- src/pages/expression_tree.jsx
+++ src/pages/expression_tree.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import "./css/exptree.css";
 
 function ExpressionTree() {
   return (
     <>
-      <h1>Expression</h1>
+      <div className="main-div">
+        <div className="headingdiv">
+          <h2 className="main_heading">Expression Tree</h2>
+        </div>
+        <div className="input-div">
+          <label style={{ alignSelf: "flex-start" }} className="main_label">
+            Enter Expression
+          </label>
+          <input className="expinput" maxLength={10} />
+          <label className="warning_label">*Maximum characters 10</label>
+          <button title="Submit" name="Submit" className="submitbtn">
+            Submit
+          </button>
+        </div>
+        <hr />
+      </div>
     </>
   );
 }
