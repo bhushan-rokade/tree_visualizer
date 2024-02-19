1d8b9478af5f603b79b493bb898acbb6c3b517f8
Bhushan Rokade <83108525+http-bhushan@users.noreply.github.com>
2024-02-19 15:04:02 +0530

    changes in tree

--- src/pages/expression_tree.jsx
+++ src/pages/expression_tree.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./css/exptree.css";
+import "./css/tree.css";
 
 function ExpressionTree() {
   return (
@@ -19,6 +20,42 @@ function ExpressionTree() {
           </button>
         </div>
         <hr />
+        <div className="tree">
+          <div className="row">
+            <div className="node">1</div>
+          </div>
+          <div className="divider_div">
+            <div className="row_divider_left" />
+            <div className="row_divider_right" />
+          </div>
+
+          <div className="row">
+            <div className="node">2</div>
+            <div className="node">3</div>
+          </div>
+          <div className="divider_div2">
+            <div className="row_divider_left3" />
+            <div className="row_divider_right3" />
+            <div className="row_divider_left4" />
+            <div className="row_divider_right4" />
+          </div>
+          <div className="row">
+            <div className="node">4</div>
+            <div className="node">5</div>
+            <div className="node">6</div>
+            <div className="node">7</div>
+          </div>
+          <div className="row">
+            <div className="node">8</div>
+            <div className="node">9</div>
+            <div className="node">10</div>
+            <div className="node">11</div>
+            <div className="node">12</div>
+            <div className="node">13</div>
+            <div className="node">14</div>
+            <div className="node">15</div>
+          </div>
+        </div>
       </div>
     </>
   );
