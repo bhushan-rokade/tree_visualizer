80652c06cd950b9ba3fbd1c0758525cdc336ef1d
Bhushan Rokade <83108525+http-bhushan@users.noreply.github.com>
2024-07-19 22:35:06 +0530

    fixed some logical errors

--- src/pages/BinaryTree.jsx
+++ src/pages/BinaryTree.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
-import "./css/exptree.css";
-import "./../Components/componentStyles/tree.css";
-import Tree from "../Components/BSTree";
-import { handleSearch2, handleSubmit } from "../Components/Bi_Tree_functions";
+import React, { useState } from 'react';
+import './css/exptree.css';
+import './../Components/componentStyles/tree.css';
+import Tree from '../Components/BSTree';
+import { handleSearch2, handleSubmit } from '../Components/Bi_Tree_functions';
 
 function BinaryTree() {
   const [disabled, setDisabled] = useState(false);
-
+  const [buidlOn, setBuildOn] = useState(false);
   const [numbers, setNumbers] = useState(0);
 
   const handleSearch = () => {
     const result = handleSearch2(numbers);
 
     if (result == 0) {
-      alert("element not found");
+      alert('element not found');
       location.reload();
     }
   };
@@ -23,52 +23,53 @@ function BinaryTree() {
   };
   return (
     <>
-      <div className="main-div">
-        <div className="controls_div">
-          <div className="headingdiv">
-            <h2 className="main_heading">Binary Search Tree</h2>
-            <h4 className="intro_text">
+      <div className='main-div'>
+        <div className='controls_div'>
+          <div className='headingdiv'>
+            <h2 className='main_heading'>Binary Search Tree</h2>
+            <h4 className='intro_text'>
               Here we have numbers [8,3,1,6,4,7,10,14,13] represented in Data
               Structure Binary Search Tree.
             </h4>
           </div>
-          <div className="buttons_div">
+          <div className='buttons_div'>
             <label>Click To Start Tree Building</label>
             <button
-              title="Submit"
+              title='Submit'
               onClick={handleSubmit}
-              name="Submit"
-              className="buildbtn">
+              name='Submit'
+              disabled={buidlOn}
+              className='buildbtn'>
               Build Tree
             </button>
           </div>
-          <hr style={{ width: "100%" }} />
-          <div id="input_div" style={{ display: "none" }}>
-            <div id="inner_inputdiv">
-              <label id="searchlabel">Enter number for searching</label>
+          <hr style={{ width: '100%' }} />
+          <div id='input_div' style={{ display: 'none' }}>
+            <div id='inner_inputdiv'>
+              <label id='searchlabel'>Enter number for searching</label>
               <input
-                id="search_input"
-                style={{ textAlign: "center" }}
-                defaultValue=""
+                id='search_input'
+                style={{ textAlign: 'center' }}
+                defaultValue=''
                 onChange={(e) => setNumbers(e.target.value)}
                 disabled={disabled}
               />
               <div
                 style={{
-                  height: "70%",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
+                  height: '70%',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
                 }}>
                 <button
-                  id="searchbtn"
+                  id='searchbtn'
                   onClick={() => {
                     setDisabled(true);
                     handleSearch();
                   }}>
                   Search
                 </button>
-                <button id="clearbtn" onClick={handleClear}>
+                <button id='clearbtn' onClick={handleClear}>
                   Clear
                 </button>
               </div>
